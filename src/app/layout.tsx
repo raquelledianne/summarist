f749ec32b2df16b3afe2a919c7695f8efeb9afbd
raquelledianne <raquelledschoolcraft@gmail.com>
2026-03-20 06:01:24 -0700

    save2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,32 @@
 "use client";
 
 import "./globals.css";
+
+
 import { useState } from "react";
-import { usePathname } from "next/navigation";
 import Sidebar from "../components/Sidebar";
 import AuthModal from "../modals/AuthModal";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [showSidebar, setShowSidebar] = useState(true);
   const [isAuthOpen, setIsAuthOpen] = useState(false);
-  const pathname = usePathname();
-
- 
-  const noSidebarPages = ["/choose-plan", "/checkout"];
-  const showSidebar = !noSidebarPages.includes(pathname);
 
   return (
     <html lang="en">
       <body>
         
-        {showSidebar && <Sidebar onLoginClick={() => setIsAuthOpen(true)} />}
+        {showSidebar && <Sidebar />}
+
+
+        
+        <div className="wrapper">
+          <div className="row">
+            <div className="container">{children}</div>
+          </div>
+        </div>
 
         
         <AuthModal isOpen={isAuthOpen} onClose={() => setIsAuthOpen(false)} />
-
-        
-        {children}
       </body>
     </html>
   );
